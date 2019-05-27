91f740eb1bf1f0359de8918313dae8418baa70c1
Nico <5westbury5@gmail.com>
2019-05-27 01:52:36 -0700

    Title processing mostly done, still need to generalize, add comment processing

--- test.js
+++ test.js
@@ -1,4 +1,5 @@
 const puppeteer = require('puppeteer');
+const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 const fs = require('fs');
 
 const CONFIG = require('./config.json');
@@ -25,6 +26,29 @@ const login_reddit = async (page) => {
     return cookies;
 }
 
+const sentence_regex = /([^,\.!\?;]+[,\.!\?;]+)|([^,\.!\?;]+$)/g;
+const split_paragraphs = (paras) => {
+    let flattened_sentences = [];
+    const para_sentences = paras.map((para) => {
+        const split = para.match(sentence_regex);
+
+        let prev = "";
+        const sentences = split.map((sentence) => {
+            prev += sentence;
+            return prev;
+        });
+
+        flattened_sentences = flattened_sentences.concat(split);
+
+        return sentences;
+    });
+
+    return [flattened_sentences, para_sentences];
+}
+const addslashes = (str) => (str + '').replace(/[\\"']/g, '\\$&').replace(/\u0000/g, '\\0');
+
+const exposed_funcs = [split_paragraphs];
+
 
 (async () => {
     const browser = await puppeteer.launch({headless: false});
@@ -34,22 +58,30 @@ const login_reddit = async (page) => {
 
     const page = (await browser.pages())[0];
     page.setDefaultTimeout(60000);
+    page.on('console', consoleObj => console.log(consoleObj.text()));
 
+    await page.setViewport({ width: 800, height: 600, deviceScaleFactor: 2 })
+
+    // Functions need to be explicitely expose to be acessed within the node context
+    exposed_funcs.forEach(async (func) => {
+        await page.exposeFunction(func.name, func);
+    });
 
     const cookies = await login_reddit(page);
-    console.log('test', cookies);
     await page.setCookie(...cookies);
 
     const subreddit = 'uwaterloo';
     const topDivs = `div.SubredditVars-r-${subreddit}`;
-    await page.goto('https://www.reddit.com/r/uwaterloo/comments/9uhk57/admissions_megathread_fall_2019_incoming_students/?sort=top',
+    await page.goto('https://www.reddit.com/r/uwaterloo/comments/9uhk57/?sort=top',
                     {waitUntil: 'domcontentloaded'});
     await page.waitForSelector(`${topDivs}:nth-of-type(2) div`);
 
+    // Up vote page :)
     if (await page.$('[data-click-id="upvote"][aria-pressed="false"][id^="upvote-button-"]') !== null) {
         await page.click('[data-click-id="upvote"][aria-pressed="false"][id^="upvote-button-"]');
     }
 
+    // Delete stuff from the page that is unnecessary
     await page.evaluate((topDivs) => {
         // Remove margin
         let dom = document.querySelector(`${topDivs}:nth-of-type(2) div`);
@@ -70,22 +102,59 @@ const login_reddit = async (page) => {
 
     await page.waitFor(500); // Timeout hack to get shit to work (why do I need this?)
 
+    // Take screenshot of just the title and upvote buttons
     const selector = `${topDivs}:nth-of-type(1) > div > div > div > div:nth-of-type(2) > div:nth-of-type(2) > div > div > div`;
-    console.log('test', selector);
     const largeDiv = await page.$$(selector);
-    console.log('se', largeDiv.length);
-    await largeDiv[0].screenshot({path: 'title.png'});
-    await largeDiv[3].screenshot({path: 'example1.png'});
+    const titleDiv = largeDiv[0];
+    await titleDiv.screenshot({path: 'img/title.jpeg', quality: 100});
 
-    await page.evaluate((topDivs) => {
+    // Re-display the post-content text and comment numbers
+    await page.evaluate(() => {
         let nodes = document.querySelectorAll('div[data-test-id="post-content"] > div');
         nodes[4].style['display'] = 'block';
-        nodes[5].style['display'] = 'block';
-    }, topDivs);
+        // nodes[5].style['display'] = 'block';
+    });
 
-    await largeDiv[0].screenshot({path: 'test1.png'});
+    const nodes = await page.$$('div[data-test-id="post-content"] > div');
+    const title = await page.evaluate(div => div.textContent.trim(), nodes[2]);
+    const pars = await page.evaluateHandle(div => div.querySelectorAll('div > p'), nodes[4]);
+
+    // Hide paragraphs, get text
+    const [split_sentences, para_sentences] = await page.evaluate((pars) => {
+        const text = [];
+        for (const p of pars) {
+            text.push(p.textContent);
+            p.style['display'] = 'none';
+        }
+        return split_paragraphs(text);
+    }, pars);
 
 
+    const rows = [{name: 'title', path: 'img/title.jpeg', text: title}];
+
+    let sentence_index = 0;
+    for (var i=0; i<para_sentences.length; ++i) {
+        // Un-hide individual paragraphs
+        await page.evaluate((pars, i) => { pars[i].style['display'] = 'block'; }, pars, i);
+
+        for (const para_sentence of para_sentences[i]) {
+            const path = `img/post_frame${sentence_index}.jpeg`;
+            const text = split_sentences[sentence_index].trim();
+            // Change the text of the current paragraph to add in the next sentence
+            await page.evaluate((pars, i, para_sentence) => { pars[i].innerHTML = para_sentence }, pars, i, para_sentence);
+            await titleDiv.screenshot({path, quality: 100});
+
+            rows.push({name: `post_frame${sentence_index}`, path, text});
+            ++sentence_index;
+        }
+    }
+
+    const csvWriter = createCsvWriter({  
+        path: 'text/9uhk57.csv',
+        header: [{id: 'name', title: 'Name'}, {id: 'path', title: 'Path'}, {id: 'text', title: 'Text'}]
+    });
+
+    await csvWriter.writeRecords(rows);
 
     /*
     const text = await page.evaluate(async (selector) => {
