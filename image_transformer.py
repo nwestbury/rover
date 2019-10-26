ae9ee4281a4d838befc3fc3696ae08e190fd2d21
Nico <5westbury5@gmail.com>
2019-10-26 13:25:22 -0700

    image_transformer changes

--- image_transformer.py
+++ image_transformer.py
@@ -2,15 +2,22 @@ from PIL import Image
 
 class ImageTransformer():
     def __init__(self):
-        self.width = 1080
-        self.height = 1980
+        self.height = 1080
+        self.width = 1980
         self.background_color = (26, 26, 26)
 
-    def save_new_image(self, img, path):
+    def save_new_image(self, img, y = None):
         image = Image.open(img)
-        new_image = Image.new('RGB', (self.height, self.width), self.background_color)
-        new_image.paste(image)
-        new_image.save(path)
+        if image.width > self.width or image.height > self.height:
+            ratio = min(self.width / image.width, self.height / image.height)
+            image = image.resize((self.width * ratio, self.height * ratio))
+
+        new_image = Image.new('RGB', (self.width, self.height), self.background_color)
+
+        y = self.height // 2 - image.height // 2 if y is None else y
+        box = (self.width // 2 - image.width // 2, y)
+        new_image.paste(image, box=box)
+        new_image.save('test.jpeg')
 
 
 if __name__ == '__main__':
