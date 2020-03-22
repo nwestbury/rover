efb77342aca5c4ebd4030bf4e59674ac48e38bda
Nico <5westbury5@gmail.com>
2020-03-22 07:24:36 -0700

    100 max length for upload better cut-off

--- youtube_uploader.py
+++ youtube_uploader.py
@@ -1,5 +1,8 @@
 import os
+import logging
 from youtube_upload import initialize_upload, upload_thumbnail
+
+logger = logging.getLogger('youtube_uploader')
 
 class YouTubeUploader():
     def __init__(self):
@@ -8,13 +11,25 @@ class YouTubeUploader():
     def upload_thumbnail(self, thumbNailPath, video_id):
         upload_thumbnail(video_id, thumbNailPath)
 
+    def compute_shorter_name(self, postTitle, maxLength):
+        if len(postTitle) > maxLength:
+            return postTitle[:(maxLength - 3)] + '...'
+
+        return postTitle[:maxLength]
+
     def upload(self, videoPath, postTitle, subReddit, postList, credits={}):
-        title = f'{postTitle[:65]} (r/{subReddit[:14]} | Reddit Rover)' # needs to be <= 100 chars
+        subRedditShortenedName = subReddit[:14]
+        postMaxLength = 65 + (14 - len(subRedditShortenedName))
+        postTitleShortenedName = self.compute_shorter_name(postTitle, postMaxLength)
+
+        title = f'{postTitleShortenedName} (r/{subRedditShortenedName} | Reddit Rover)' # needs to be <= 100 chars
         title = title.replace('<', '').replace('>', '')
+
+        logger.warning('Uploading video called "%s"', title)
 
         extra_credits = os.linesep + os.linesep.join(f'{key}: {link}' for key, link in credits.items())
 
-        description = f'''► {title}
+        description = f'''► {postTitle}
 ► I am Reddit Rover, I explore Reddit and find the highlights for your enjoyment! New videos every day.
 
 ⭐ Click the Subscribe button to get video in your feed
@@ -37,4 +52,4 @@ Posts:
 
 if __name__ == "__main__":
     ytu = YouTubeUploader()
-    ytu.upload('Omg title', 'AskReddit', ['1:03 - link 1', '1:04 - link 2'])
+    ytu.upload('video/out.mp4', 'Omg title' * 100, 'AskReddit', ['1:03 - link 1', '1:04 - link 2'])
